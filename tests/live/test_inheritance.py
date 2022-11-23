c1450495731c50c77966f4e8f09463ef4424006d
David C Ellis <ducksual@gmail.com>
2022-11-23 18:13:05 +0000

    Black formatting

--- tests/live/test_inheritance.py
+++ tests/live/test_inheritance.py
@@ -56,7 +56,7 @@ def test_layered_inheritance():
 
     x = Coordinate4D(1, 2, 3, 4)
 
-    assert x.PREFAB_FIELDS == ['x', 'y', 'z', 't']
+    assert x.PREFAB_FIELDS == ["x", "y", "z", "t"]
 
     assert (x.x, x.y, x.z, x.t) == (1, 2, 3, 4)
 
