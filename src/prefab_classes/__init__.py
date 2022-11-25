68b60604b56ae42e9af27ab215e9591a20ba7e7a
David C Ellis <ducksual@gmail.com>
2022-11-25 16:53:38 +0000

    Move to_json, as_dict into .funcs and add them to __init__ to allow import from the main module.

--- src/prefab_classes/__init__.py
+++ src/prefab_classes/__init__.py
@@ -1,7 +1,7 @@
-__version__ = "v0.6.0-alpha.4"
+__version__ = "v0.6.0-alpha.5"
 PREFAB_MAGIC_BYTES = b"_".join([b"PREFAB_CLASSES", __version__.encode()])
 
 from .live import prefab, attribute
 from .compiled import prefab_compiler
 from .exceptions import PrefabError
-from .funcs import is_prefab
+from .funcs import is_prefab, to_json, as_dict
--- src/prefab_classes/funcs.py
+++ src/prefab_classes/funcs.py
@@ -3,3 +3,52 @@ from .constants import FIELDS_ATTRIBUTE
 
 def is_prefab(o):
     return hasattr(o, FIELDS_ATTRIBUTE)
+
+
+def as_dict(inst, *, excludes=None):
+    """
+    Represent the prefab as a dictionary of attribute names and values.
+
+    :param inst: Intance of prefab class
+    :param excludes: list or set of values to exclude from the resulting dict
+    :return: dictionary {attribute_name: attribute_value, ...}
+    """
+    result = {}
+    excludes = excludes or set()
+
+    attrib_names = getattr(inst, FIELDS_ATTRIBUTE)
+
+    for name in attrib_names:
+        if name not in excludes:
+            value = getattr(inst, name)
+            result[name] = value
+    return result
+
+
+def to_json(inst, *, excludes=None, indent=2, default=None, **kwargs):
+    """
+    Output the class attributes as JSON
+    :param inst: Instance of prefab class
+    :param excludes: list or set of attributes to exclude from json dump
+    :param indent: indent for json
+    :param default: default function for JSON Encoder
+    :return:
+    """
+
+    out_dict = as_dict(inst, excludes=excludes)
+
+    # This function tells the JSON encoder how to serialise Prefab derived objects
+    # If the user needs to serialize other classes their default will be called
+    # only if the object is not an instance of Prefab
+    def default_func(o):
+        if is_prefab(o):
+            return as_dict(o)
+        elif default is not None:
+            return default(o)
+        raise TypeError(
+            f"Object of type {o.__class__.__name__} is not JSON Serializable"
+        )
+
+    import json  # Only import JSON if needed
+
+    return json.dumps(out_dict, indent=indent, default=default_func, **kwargs)
