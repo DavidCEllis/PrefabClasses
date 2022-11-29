4dff6e214b4b43b05eac8d207a418cb72c2ed5ab
David C Ellis <ducksual@gmail.com>
2022-11-29 10:52:15 +0000

    Add preview_source function that operates directly on source text.

--- src/prefab_classes/compiled/__init__.py
+++ src/prefab_classes/compiled/__init__.py
@@ -1,2 +1,2 @@
 from .import_hook import prefab_compiler
-from .preview_source import preview
+from .preview_source import preview_source, preview
--- src/prefab_classes/compiled/preview_source.py
+++ src/prefab_classes/compiled/preview_source.py
@@ -2,21 +2,8 @@ import ast
 import os
 
 
-def preview(pth: os.PathLike, use_black: bool = True):
-    """
-    Preview the result of running the generator on a python file
-    This is mainly here for debugging and testing but can also be useful
-    if users are unsure how the class will be generated.
-
-    :param pth: Path to the .py
-    :param use_black: use the black formatter to make the result easier to read
-                      if black is installed.
-    :return: string output of generated python code from the AST
-    """
+def preview_source(source: str, use_black: bool = True):
     from .generator import compile_prefabs
-
-    with open(pth, mode="r", encoding="utf-8") as f:
-        source = f.read()
 
     tree = compile_prefabs(source)
     # Black usage is not covered in case black changes formatting.
@@ -29,3 +16,20 @@ def preview(pth: os.PathLike, use_black: bool = True):
             return ast.unparse(tree)
     else:
         return ast.unparse(tree)
+
+
+def preview(pth: os.PathLike, use_black: bool = True):
+    """
+    Preview the result of running the generator on a python file
+    This is mainly here for debugging and testing but can also be useful
+    if users are unsure how the class will be generated.
+
+    :param pth: Path to the .py
+    :param use_black: use the black formatter to make the result easier to read
+                      if black is installed.
+    :return: string output of generated python code from the AST
+    """
+    with open(pth, mode="r", encoding="utf-8") as f:
+        source = f.read()
+
+    return preview_source(source, use_black=use_black)
