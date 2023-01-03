7bfca460ba479574daff3f068beb2540774443c3
davidcellis <ducksual@gmail.com>
2023-01-03 16:16:46 +0000

    Compiled test for slotted classes

--- tests/compiled/compile_targets/example_slots.py
+++ tests/compiled/compile_targets/example_slots.py
@@ -1,5 +1,5 @@
 # COMPILE_PREFABS
-from prefab_classes import prefab
+from prefab_classes import prefab, attribute
 
 
 @prefab(compile_prefab=True, compile_slots=True)
@@ -11,3 +11,10 @@ class Coordinate:
 @prefab(compile_prefab=True, compile_slots=True)
 class Coordinate3D(Coordinate):
     z: float
+
+
+@prefab(frozen=True, compile_prefab=True, compile_slots=True)
+class FrozenExample:
+    x: int
+    y: str = "Example Data"
+    z: list = attribute(default_factory=list)
