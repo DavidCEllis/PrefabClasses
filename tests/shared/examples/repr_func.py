2e374488d0532824fd4519d3f2a3c0c59301b538
David C Ellis <ducksual@gmail.com>
2023-01-23 12:53:22 +0000

    Test for the alternative repr when conditions are correct.

---
+++ tests/shared/examples/repr_func.py
@@ -0,0 +1,36 @@
+# COMPILE_PREFABS
+from prefab_classes import prefab, attribute
+
+
+@prefab(compile_prefab=True, compile_fallback=True)
+class RegularRepr:
+    x: str = "Hello"
+    y: str = "World"
+
+
+@prefab(compile_prefab=True, compile_fallback=True)
+class OneAttributeNoRepr:
+    x: str = attribute(default="Hello", repr=False)
+    y: str = "World"
+
+
+@prefab(compile_prefab=True, compile_fallback=True)
+class OneAttributeNoInit:
+    x: str = "Hello"
+    y: str = attribute(default="World", init=False)
+
+
+@prefab(compile_prefab=True, compile_fallback=True)
+class OneAttributeExcludeField:
+    x: str = "Hello"
+    y: str = attribute(default="World", exclude_field=True)
+
+    def __prefab_post_init__(self, y):
+        self.y = y
+
+
+@prefab(compile_prefab=True, compile_fallback=True)
+class RegularReprOneArg:
+    x: str = "Hello"
+    y: str = attribute(default="World", init=False, repr=False)
+
---
+++ tests/shared/test_repr.py
@@ -0,0 +1,26 @@
+import pytest
+
+def test_basic_repr(importer):
+    from repr_func import RegularRepr
+    x = RegularRepr()
+    assert repr(x) == "RegularRepr(x='Hello', y='World')"
+
+def test_one_attribute_no_repr(importer):
+    from repr_func import OneAttributeNoRepr
+    x = OneAttributeNoRepr()
+    assert repr(x) == "<prefab OneAttributeNoRepr; y='World'>"
+
+def test_one_attribute_no_init(importer):
+    from repr_func import OneAttributeNoInit
+    x = OneAttributeNoInit()
+    assert repr(x) == "<prefab OneAttributeNoInit; x='Hello', y='World'>"
+
+def test_one_attribute_exclude_field(importer):
+    from repr_func import OneAttributeExcludeField
+    x = OneAttributeExcludeField()
+    assert repr(x) == "<prefab OneAttributeExcludeField; x='Hello'>"
+
+def test_regular_one_arg(importer):
+    from repr_func import RegularReprOneArg
+    x = RegularReprOneArg()
+    assert repr(x) == "RegularReprOneArg(x='Hello')"
