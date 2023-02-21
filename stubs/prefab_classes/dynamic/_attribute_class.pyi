02d1f47b953956f4536b87d5f2032aec40207787
David C Ellis <ducksual@gmail.com>
2023-02-21 20:03:43 +0000

    Update more stubs, still fails

--- stubs/prefab_classes/dynamic/_attribute_class.pyi
+++ stubs/prefab_classes/dynamic/_attribute_class.pyi
@@ -1,5 +1,6 @@
 from ..exceptions import LivePrefabError as LivePrefabError
 from ..sentinels import NOTHING as NOTHING
+from ..sentinels import _NOTHING_TYPE
 import typing
 from _collections_abc import Callable
 
@@ -8,18 +9,21 @@ class Attribute:
     PREFAB_FIELDS: list[str]
     __match_args__: tuple[str, ...]
     default: typing.Any
-    default_factory: Callable[[], typing.Any]
+    default_factory: Callable[[], typing.Any] | _NOTHING_TYPE
     init: bool
     repr: bool
+    compare: bool
     kw_only: bool
     exclude_field: bool
-    _type: str | type
+    _type: str | type | _NOTHING_TYPE
     def __init__(
         self,
+        *,
         default=...,
         default_factory=...,
         init: bool = ...,
         repr: bool = ...,
+        compare: bool = ...,
         kw_only: bool = ...,
         exclude_field: bool = ...,
     ) -> None: ...
