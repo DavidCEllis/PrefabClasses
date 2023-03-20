1796ec4b9e1a36c545c63716f33d7c051ec68d7e
David C Ellis <ducksual@gmail.com>
2023-03-20 10:56:43 +0000

    use !r for repr instead of putting in single quotes by hand

--- src/prefab_classes/dynamic/method_generators.py
+++ src/prefab_classes/dynamic/method_generators.py
@@ -255,7 +255,7 @@ def get_frozen_setattr_maker():
         field_names = getattr(cls, FIELDS_ATTRIBUTE)
 
         # Make the fields set literal
-        fields_delimited = ", ".join(f"'{field}'" for field in field_names)
+        fields_delimited = ", ".join(f"{field!r}" for field in field_names)
         field_set = f"{{ {fields_delimited} }}"
 
         # Dynamic prefabs are not slotted so it is possible to insert into the dict
