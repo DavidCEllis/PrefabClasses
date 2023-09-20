459c66e83f4971fc12d53de8867c7b7ae523bb3a
David C Ellis <ducksual@gmail.com>
2023-09-20 12:14:49 +0200

    Bump version number

--- src/prefab_classes/__init__.py
+++ src/prefab_classes/__init__.py
@@ -1,5 +1,5 @@
-__version__ = "v0.9.4"
-PREFAB_MAGIC_BYTES = b"PREFAB_CLASSES_v0.9.4"
+__version__ = "v0.9.5"
+PREFAB_MAGIC_BYTES = b"PREFAB_CLASSES_v0.9.5"
 
 from .dynamic import prefab, attribute, build_prefab
 from .sentinels import KW_ONLY
