a0af9ba9230b97f705276eac7ec36c892e03eba1
David C Ellis <ducksual@gmail.com>
2024-04-03 13:00:43 +0100

    Update/Add copyright notices

--- src/prefab_classes/_class_generator.py
+++ src/prefab_classes/_class_generator.py
@@ -1,5 +1,5 @@
 # ==============================================================================
-# Copyright (c) 2022 David C Ellis
+# Copyright (c) 2022-2024 David C Ellis
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
